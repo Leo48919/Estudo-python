af085cd48283e0cafbbc5bc2246525fc45cf1075
Flying <contatoflyingmm@gmail.com>
2026-02-26 13:33:41 -0300

    subida aulas python

---
+++ laco_repetição.py
@@ -0,0 +1,25 @@
+# x = 1 
+
+# while x < 10:
+#     print(x)
+#     x += 1
+
+# aprendendo conceitos laço for  ===========================
+
+# for i in range(10):
+#     print(i)
+
+# lista1 = [1, 2, 3, 4, 5]
+
+# lista2 = ["ola", "mundo","!"]
+
+# lista3 = [1, "ola", 3.14, True]
+
+# for i in lista3:
+#     print(i)
+
+
+#combinar função rang'e com listas ===========================
+
+for i in range(10, 20):
+    print(i)
---
+++ string.py
@@ -0,0 +1,16 @@
+# a = "meu nome é "
+# b = "Leo"
+
+# print(a + b)
+
+
+# seq = "Python é uma linguagem de programação incrível!  "
+
+# letras = seq[0:6]
+# print(letras)
+
+
+minha_string =" o rato roeu a roupa do rei de roma "
+
+minha_lista = minha_string.split()
+print(minha_lista)
